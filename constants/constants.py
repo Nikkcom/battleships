54f0286f80e1585da0f141be9a8ca186709aebfc
Nikkcom <82175612+Nikkcom@users.noreply.github.com>
2023-12-19 00:42:29 +0100

    Create constants.py

---
+++ constants/constants.py
@@ -0,0 +1,16 @@
+SHIPS = {
+    "Aircraft Carrier": 6,
+    "Frigate": 5,
+    "Attack Cruiser": 4,
+    "Combat Ship": 3,
+    "Patrol Boat": 2,
+    "Attack RIB": 1
+}
+SHIP_CHARS = {
+    "Aircraft Carrier": "6",
+    "Frigate": "5",
+    "Attack Cruiser": "4",
+    "Combat Ship": "3",
+    "Patrol Boat": "2",
+    "Attack RIB": "1"
+}
